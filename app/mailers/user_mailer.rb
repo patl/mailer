f32fb43a362624533822bf728b3913d05cba6160
patl <ogboiko@gmail.com>
2012-02-06 17:57:49 +0200

    first commit

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ActionMailer::Base
+  default :from => "ogboiko@gmail.com	"
+
+  def welcome_email(user)
+    @user = user
+    @url  = "http://example.com/login"
+    mail(:to => user.email,
+         :subject => "Welcome to My Awesome Site")
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailer::Application.config.secret_token = '1c8267df2d463f3656a903217eaf9467c1c01622e7a0ea15bd5671ef1cdde70e2cee5f7546a089a62f9482f5f5f83b0e12bb986888c58b93608ae5ab5a690922'
